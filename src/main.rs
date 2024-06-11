1092f09109c8eed91946cc628282fcf72466f8a9
ochioci <63116595+ochioci@users.noreply.github.com>
2024-06-11 13:39:07 -0400

    fsdfsdf

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
+#![allow(warnings)]
+
 fn main() {
     println!("Hello, world!");
     struct Game {
         game_state: State,
-        left: [u8; 6],
-        right: [u8; 6],
-        left_pit: u8,
-        right_pit: u8,
+        board: [u8; 14],
     }
-    #[derive(Default)]
+    #[derive(Default, Debug)]
     enum State {
         #[default]
         LeftToMove,
@@ -16,46 +15,105 @@ fn main() {
     }
 
     impl Game {
+
         pub fn display(&self) {
-            println!("    {:?}    ", self.left_as_string());
-            println!("  {:?}                    {:?}", self.left_pit, self.right_pit);
-            println!("    {:?}    ", self.right_as_string());
-        }
-
-        fn left_as_string(&self) -> String {
-            let mut out = String::from("");
-            for i in 0..(self.left.len()) {
-                out += &self.right[i].to_string();
-                out += "  ";
+            let mut top = String::from("  ");
+            let mut bottom = String::from("  ");
+            for i in 0..6 {
+                top += &self.board[i].to_string();
+                top += &"  ";
+                bottom += &self.board[12-i].to_string();
+                bottom += &"  ";
             }
-            out
+            println!("{:?}", self.game_state);
+            println!("{}", top);
+            println!("{}                  {}", self.board[13], self.board[6]);
+            println!("{}", bottom);
+            println!();
         }
-        fn right_as_string(&self) -> String {
-            let mut out = String::from("");
-            for i in 0..(self.left.len()) {
-                out += &self.right[i].to_string();
-                out += "  ";
+
+
+    }
+
+    impl Game {
+
+        fn swap_turn(&mut self) {
+            match self.game_state {
+                State::LeftToMove => self.game_state = State::RightToMove,
+                State::RightToMove => self.game_state = State::LeftToMove,
+                _ => return
             }
-            out
         }
 
-        pub fn leftMove(&self) {
+        fn make_move(&mut self, target : u8) {
+            let mut marbles = self.board[target as usize];
+            self.board[target as usize] = 0;
+            let mut index : i8 = target as i8;
+            match self.game_state {
+                State::LeftToMove => index+=1,
+                State::RightToMove => index-=1,
+                _ => return
+            }
+            let (pit, opp_pit) = match self.game_state {
+                State::LeftToMove => (6, 13),
+                State::RightToMove => (13, 6),
+                _ => return
+            };
+            while (marbles > 0) {
+                if index != opp_pit {
+                    self.board[index as usize] +=1;
+                }
+                marbles -=1;
+                if (index == 0) {
+                    index = 13;
+                } else if (index == 13) {
+                    index = 0
+                } else {
+                    match self.game_state {
+                        State::LeftToMove => index+=1,
+                        State::RightToMove => index-=1,
+                        _ => return
+                    }
+                }
+            };
+            match self.game_state {
+                State::LeftToMove => index-=1,
+                State::RightToMove => index+=1,
+                _ => return
+            }
+            if (index != pit) { //free turn mechanic
+                self.swap_turn();
+            }
+            // println!("{}     {}", self.board[index as usize], index);
+            if self.board[index as usize] == 1 &&
+                (match self.game_state {
+                    State::RightToMove => ((0..6).contains(&index)),
+                    State::LeftToMove => ((7..13).contains(&index)),
+                    _ => return
 
+                }) { //steal mechanic
+                self.board[pit as usize] += self.board[index as usize] + self.board[(index - 12).abs() as usize];
+                self.board[index as usize] = 0;
+                self.board[(index-12).abs() as usize] = 0;
+            }
         }
     }
+
     impl Default for Game {
         fn default() -> Self {
             Self {
-                left: [4;6],
-                right: [4;6],
-                left_pit: 0,
-                right_pit: 0,
+                board: [4,4,4,4,4,4,0,4,4,4,4,4,4,0],
                 game_state: State::default()
             }
         }
     }
 
-    let test_game : Game = Default::default();
+    let mut test_game: Game = Default::default();
+    test_game.display();
+    // test_game.board[4] = 0;
+    test_game.make_move(1);
+    test_game.display();
+    test_game.make_move(9);
     test_game.display();
 
 }

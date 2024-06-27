9fb67a39ca3206c217014db08708497978af9a7c
ochioci <63116595+ochioci@users.noreply.github.com>
2024-06-26 20:40:53 -0400

    Some CLI changes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![allow(warnings)]
 
 use crate::ai::{best_move, eval};
-use crate::game::Game;
+use crate::game::{Game, State};
 use std::env;
 use crate::test::test_ai;
 
@@ -13,7 +13,7 @@ mod test;
 static CLEAR: bool = true;
 static LEFT_AI: bool = true;
 static RIGHT_AI: bool = false;
-static AI_DEPTH: u8 = 9;
+static AI_DEPTH: u8 = 6;
 static THREAD_DEBUG: bool = false;
 static AI_DEBUG: bool = false;
 
@@ -22,20 +22,32 @@ fn main() {
         print!("{esc}c", esc = 27 as char);
     };
     let args: Vec<String> = env::args().collect();
-    let mut prevArg = String::from("");
-    let mut doCli = false;
-    let mut cliInput = String::from("");
+    let mut prev_arg = String::from("");
+    let mut play_best_move = false;
+    let mut cli_input = String::from("");
     for a in &args {
-        if prevArg == "cli" {
+        if prev_arg == "pbm" {
             // println!("{}", a);
-            doCli = true;
-            cliInput = a.clone();
+            play_best_move = true;
+            cli_input = a.clone();
         }
-        prevArg = a.clone();
+        prev_arg = a.clone();
     }
-    if doCli {
-        let g = Game::from(&cliInput);
-        println!("{:?}", best_move(g, eval))
+    if play_best_move {
+        let mut g = Game::from(&cli_input);
+        let bestMove = best_move(g.clone(), eval);
+        // println!("{}", String::from(&g));
+        println!("{:?}", bestMove);
+        match g.get_state() {
+            State::LeftToMove => {
+                g.make_move(bestMove.0 - 1);
+            }
+            State::RightToMove => {
+                g.make_move(13- bestMove.0);
+            }
+            _ => {}
+        }
+        println!("{}", String::from(&g));
     } else {
         test_ai(eval, eval);
     }
